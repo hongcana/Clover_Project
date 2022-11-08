6df43ed35ec555aa635909f061b174b95cdd195d
박예한 <55950561+pyh-dotcom@users.noreply.github.com>
2022-11-09 00:14:35 +0900

    Add files via upload

---
+++ server/src/main/java/com/example/clover_backend/repository/StockItem.java
@@ -0,0 +1,28 @@
+/*
+package com.example.clover_backend.repository;
+
+import com.example.clover_backend.dto.StockItemSearchResponse;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+@Table(name="stockitems")
+public class StockItem {
+
+    @Id
+    @Column(name="code")
+    private String code; // 종목코드
+
+    @Column(name="name")
+    private String name; // 기업이름
+
+    public StockItemSearchResponse stockItemResponse() {
+        return StockItemSearchResponse.builder()
+                .code(code)
+                .name(name)
+                .build();
+    }
+}
+ */
---
+++ server/src/main/java/com/example/clover_backend/service/StockItemSearchNotFoundException.java
@@ -0,0 +1,9 @@
+/*
+package com.example.clover_backend.service;
+
+public class StockItemSearchNotFoundException extends RuntimeException {
+    public StockItemSearchNotFoundException(String keyword) {
+        super(keyword + "not found");
+    }
+}
+ */
