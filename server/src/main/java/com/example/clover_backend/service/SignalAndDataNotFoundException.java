b7395c015a755ecde1034f4f898801580bb13ccc
박예한 <55950561+pyh-dotcom@users.noreply.github.com>
2022-11-11 02:30:40 +0900

    Delete server/src directory

--- server/src/main/java/com/example/clover_backend/service/SignalAndDataNotFoundException.java
+++
@@ -1,4 +0,0 @@
-package com.example.clover_backend.service;
-
-public class SignalAndDataNotFoundException {
-}
--- server/src/main/java/com/example/clover_backend/service/StockItemSearchNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.clover_backend.service;
-
-public class StockItemSearchNotFoundException extends RuntimeException {
-    public StockItemSearchNotFoundException(String keyword) {
-        super(keyword + "not found");
-    }
-}
