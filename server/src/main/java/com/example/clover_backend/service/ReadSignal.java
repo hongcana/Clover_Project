3daa2d6b709f52eaf5787532a25c458e74f07983
박예한 <55950561+pyh-dotcom@users.noreply.github.com>
2022-11-12 01:52:26 +0900

    Add files via upload

---
+++ server/src/main/java/com/example/clover_backend/service/ReadSignal.java
@@ -0,0 +1,47 @@
+package com.example.clover_backend.service;
+
+import com.example.clover_backend.dto.SignalResponse;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.springframework.stereotype.Service;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+@Service
+public class ReadSignal {
+    public SignalResponse readSignal() throws IOException, ParseException {
+
+        JSONParser jsonParser  = new JSONParser();
+
+        String path = "/home/ubuntu/Clover_Project/Python/signal.json";
+	String str = Files.readString(Path.of(path));
+	str = str.substring(1, str.length());
+
+        JSONObject jsonObject = (JSONObject) jsonParser.parse(str);
+
+	System.out.println(jsonObject);
+
+        String stockCode = (String) jsonObject.get("stock_code");
+        String date = (String) jsonObject.get("date");
+        long open = (Long) jsonObject.get("open");
+        long high = (Long) jsonObject.get("high");
+        long low = (Long) jsonObject.get("low");
+        long close = (Long) jsonObject.get("close");
+        long signal = (Long) jsonObject.get("signal");
+        String reliability = (String) jsonObject.get("신뢰도");
+
+        return SignalResponse.builder()
+                .stockCode(stockCode)
+                .date(date)
+                .open(open)
+                .high(high)
+                .low(low)
+                .close(close)
+                .signal(signal)
+                .reliability(reliability)
+                .build();
+    }
+}
---
+++ server/src/main/java/com/example/clover_backend/service/SignalAndDataService.java
@@ -0,0 +1,49 @@
+package com.example.clover_backend.service;
+
+import com.example.clover_backend.dto.DataResponse;
+import com.example.clover_backend.dto.PriceData;
+import com.example.clover_backend.dto.SignalResponse;
+import com.opencsv.exceptions.CsvDataTypeMismatchException;
+import com.opencsv.exceptions.CsvRequiredFieldEmptyException;
+import lombok.RequiredArgsConstructor;
+import org.apache.commons.exec.ExecuteException;
+import org.json.simple.parser.ParseException;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
+
+@Service
+@RequiredArgsConstructor
+public class SignalAndDataService {
+
+    // private final ExportCSV kospiCSV;
+    private final WriteInfo writeInfo;
+    private final ExecutePython executePython;
+    private final ReadSignal readSignal;
+    private final ReadData readData;
+
+    // 1. React에서 받은 종목코드 Python으로 넘기기
+    // 2. Python에서 받은 시그널 + _10년치 데이터_ => React로 넘기기(JSON)
+    // 3. <2>의 10년치 데이터가 업데이트 되었다면 DB upgrade
+
+    public SignalResponse getSignal(String itemCode) throws IOException, ParseException {
+        // 1-1. python에 10년 치 데이터 넘기기(.csv)
+        // kospiCSV.writeCSV();
+
+        // 1-2. python에 종목코드 넘기기(info.json)
+        writeInfo.writeStockCode(itemCode);
+
+        // python 코드 실행
+        executePython.executePython();
+
+        // react에 시그널 보내기
+        return readSignal.readSignal();
+    }
+
+    public List<HashMap<String, PriceData>> getData(String itemCode) throws IOException, ParseException {
+        return readData.readData();
+    }
+}
